eb41bf0fff73bd978cdd3e66d5a8e368b60d2514
Evgeny Myasishchev <evgeny.myasishchev@gmail.com>
2012-01-29 10:40:50 +0200

    Adding generator that triggers all.

--- lib/rails/generators/authentication_service/all_generator.rb
+++ lib/rails/generators/authentication_service/all_generator.rb
@@ -3,8 +3,10 @@ require 'rails/generators/authentication_service/authentication_service_generato
 module AuthenticationService
   module Generators
     class All < Base
-      def generate_something
-        puts "Hello"
+      def generate_all
+        ::Rails::Generators.invoke("authentication_service:account")
+        ::Rails::Generators.invoke("authentication_service:session")
+        ::Rails::Generators.invoke("authentication_service:sessions_controller")
       end
     end
   end
